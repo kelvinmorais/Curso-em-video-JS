62f406aa52bf167d8ae35cbdf60e8e0bdc753cf4
Kelvin Morais <kelvintitomorais@hotmail.com>
2023-01-23 23:20:12 -0300

    fim ex 08

--- aula11/ex08.js
+++ aula11/ex08.js
@@ -1,6 +1,15 @@
-console.log('Console ok')
+/*console.log('Console ok') //Comando Sequencial
 
 var vel = 60.5
 console.log(`A velocidade é ${vel}Km/h`)
-console.log('Dirija sempre usando o cinto de segurança!')
+console.log('Você ultrapassou a velocidade permitida. Multado')
+console.log('Dirija sempre usando o cinto de segurança!') */
+
+var vel = 12
+console.log(`A velocidade do seu Carro é ${vel}Km/h`)
+
+if (vel > 60){
+    console.log('Você ultrapassou a velocidade permitida. Multado!')
+}
+console.log('Dirija sempre usando o cinto de segurança!') 
 
